5ef304bbbbe867abd9e65aa4dbd57d0829241d96
Jason Wang <624879@pdsb.net>
2020-04-24 00:45:01 -0400

    Adding all of the Xcode files I didn't add beforehand

---
+++ Book 1/20 Final Project/20.1 RPS/20.1 RPS/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  20.1 RPS
+//
+//  Created by Student on 2020-04-01.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
