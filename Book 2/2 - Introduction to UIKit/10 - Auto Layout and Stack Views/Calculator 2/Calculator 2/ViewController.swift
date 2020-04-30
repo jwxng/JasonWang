c07605b455383d6b4ddb479a6244870bf6739b3e
Jason Wang <624879@pdsb.net>
2020-04-30 18:42:44 -0400

    no message

---
+++ Book 2/2 - Introduction to UIKit/10 - Auto Layout and Stack Views/Calculator 2/Calculator 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator 2
+//
+//  Created by Student on 2020-04-30.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Book 2/2 - Introduction to UIKit/10 - Auto Layout and Stack Views/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Student on 2020-04-29.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
